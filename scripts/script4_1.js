cb3209d78dffa32b8b89d15edeaed2a3528255db
frizar9 <danilmartynMessi@yandex.ru>
2024-12-14 08:30:08 +0700

    Add files via upload

---
+++ scripts/script4_1.js
@@ -0,0 +1,7 @@
+function print_array(){
+    const numbers = [1,2,3,4,5,6,7]
+    for (let i = 0; i < numbers.length;i++){
+        console.log(numbers[i])
+    }
+}
+//доделать
---
+++ scripts/script4_2.js
@@ -0,0 +1,8 @@
+function pow_number(){
+    let A = 2
+    let N = 4
+    for (let c = 1; c<= N;c++){
+        let result = Math.pow(A,c);
+        alert(result);
+    }
+}
